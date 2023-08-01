1ff07630957606be9818138ad61f125e3967dc19
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-08-01 12:00:03 +0100

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/design-desk.jpeg";
 
 const imageAltText = "Image of Nyuydine Bill";
 
