fc762c5756929fabde27b1ca0eb75354c1dfe5e4
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-08-01 11:44:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nyuydine Bill",
+  title: "Web Developer and Software Engineer",
+  email: "billleynyuy@gmail.com",
+  gitHub: "NyuydineBill",
+  instagram: "nyuydinebill",
+  linkedIn: "/in/nyuydine-bill-362701257",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "bill leynyuy",
+  youTube: "@nyuydinebill",
 };
 
 const primaryColor = "#4E567E";
